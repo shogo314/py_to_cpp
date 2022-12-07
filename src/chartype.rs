3b85719f391d87ef8fe091b716d13caaeb2369bb
shogo314 <shg.kawasome@gmail.com>
2022-12-07 13:20:10 +0900

    update

--- src/chartype.rs
+++ src/chartype.rs
@@ -1,3 +1,18 @@
-fn _is_letter(c: char) -> bool {
+pub fn _is_letter(c: char) -> bool {
     'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || c == '_'
+}
+pub fn _is_numeral(c: char) -> bool {
+    '0' <= c && c <= '9'
+}
+pub fn _is_separator(c: char) -> bool {
+    "(){{}}[]:;".find(c) != None
+}
+pub fn _is_operator(c: char) -> bool {
+    "+-*/%=<>,.".find(c) != None
+}
+pub fn _is_backslash(c: char) -> bool {
+    c == '\\'
+}
+pub fn _is_hash(c: char) -> bool {
+    c == '#'
 }
